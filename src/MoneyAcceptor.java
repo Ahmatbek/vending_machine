3778a9e93a3b2adbd7c26c358fede72f855333fe
Ahmatbek <tursunbaevahmatbek@gmail.com>
2024-12-13 14:59:41 +0600

    make it possible to choose payment

--- src/MoneyAcceptor.java
+++ src/MoneyAcceptor.java
@@ -10,7 +10,7 @@ public class MoneyAcceptor {
 
     private final UniversalArray<Product> products = new UniversalArrayImpl<>();
 
-//    private final CoinAcceptor coinAcceptor;
+    private final CoinAcceptor coinAcceptor;
     private final CardAcceptor cardAcceptor;
 
     private static boolean isExit = false;
@@ -23,7 +23,7 @@ public class MoneyAcceptor {
                 new Mars(ActionLetter.F, 80),
                 new Pistachios(ActionLetter.G, 130)
         });
-//        coinAcceptor = new CoinAcceptor(100);
+        coinAcceptor = new CoinAcceptor(100);
         cardAcceptor = new CardAcceptor();
 
 
@@ -32,53 +32,91 @@ public class MoneyAcceptor {
     public static void run() {
         MoneyAcceptor app = new MoneyAcceptor();
         while (!isExit) {
-            app.startSimulation();
+            String method;
+            while (true) {
+                try {
+                    System.out.println("Enter payment method (card or coin): ");
+                    Scanner scanner = new Scanner(System.in);
+                    method = scanner.nextLine();
+                    if (method.equals("card") || method.equals("coin")) {
+                        break;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    System.out.println();
+                }
+            }
+            app.startSimulation(method);
         }
     }
 
-    private void startSimulation() {
+    private void startSimulation(String method) {
         print("В автомате доступны:");
         showProducts(products);
+        String payment = "";
+        if(method.equals("coin")) {
+            payment= method;
+            print("Монет на сумму: " + coinAcceptor.getAmount());
+        }
+        else if(method.equals("card")) {
+            payment = method;
+            print("The balance in the card: "+cardAcceptor.getAmount());
+        }
 
-//        print("Монет на сумму: " + coinAcceptor.getAmount());
-        print("The balance in the card: "+cardAcceptor.getAmount());
 
         UniversalArray<Product> allowProducts = new UniversalArrayImpl<>();
-        allowProducts.addAll(getAllowedProducts().toArray());
-        chooseAction(allowProducts);
+        allowProducts.addAll(getAllowedProducts(method).toArray());
+        chooseAction(allowProducts,payment);
 
     }
 
-    private UniversalArray<Product> getAllowedProducts() {
+    private UniversalArray<Product> getAllowedProducts(String method) {
         UniversalArray<Product> allowProducts = new UniversalArrayImpl<>();
         for (int i = 0; i < products.size(); i++) {
-//            if (coinAcceptor.getAmount() >= products.get(i).getPrice()) {
-//                allowProducts.add(products.get(i));
-//            }
-            if (cardAcceptor.getAmount() >= products.get(i).getPrice()) {
-                allowProducts.add(products.get(i));
+            if(method.equals("coin")) {
+                if (coinAcceptor.getAmount() >= products.get(i).getPrice()) {
+                    allowProducts.add(products.get(i));
+                }
+            }
+            else if(method.equals("card")) {
+                if (cardAcceptor.getAmount() >= products.get(i).getPrice()) {
+                    allowProducts.add(products.get(i));
+                }
             }
         }
         return allowProducts;
     }
 
-    private void chooseAction(UniversalArray<Product> products) {
+    private void chooseAction(UniversalArray<Product> products, String method) {
 
-//        print(" a - add to balance");
+        if (method.equals("coin")) {
+            print(" a - add to balance");
+
+        }
         showActions(products);
         print(" h - Выйти");
         String action = fromConsole().substring(0, 1);
-//        coinAcceptor.coinAdding(action,coinAcceptor);
+        if (method.equals("coin")) {
+            coinAcceptor.coinAdding(action,coinAcceptor);
+        }
+
 
         if ("h".equalsIgnoreCase(action)) {
             isExit = true;
             return;
         }
-//        if(!coinAcceptor.purchasing(action,coinAcceptor, products)) {
-//            chooseAction(products);
-//        }
-       if(!cardAcceptor.purchasing(action,cardAcceptor,products)) {
-           showActions(products);
+        if(method.equals("coin")) {
+            if(!coinAcceptor.purchasing(action,coinAcceptor, products)) {
+                chooseAction(products,method);
+            }
+        }
+       if (method.equals("card")) {
+           if(!cardAcceptor.purchasing(action,cardAcceptor,products)) {
+               showActions(products);
+           }
+       }
+       else {
+           chooseAction(products,method);
        }
 
 
