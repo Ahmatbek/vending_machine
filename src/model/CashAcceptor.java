0d398bf1b52ae5316c46b64ca07e82d7e184ee90
Ahmatbek <tursunbaevahmatbek@gmail.com>
2024-12-13 18:52:59 +0600

    Update CashAcceptor.java

--- src/model/CashAcceptor.java
+++ src/model/CashAcceptor.java
@@ -4,15 +4,19 @@ import java.util.Scanner;
 
 public class CashAcceptor {
     private int amount;
+    
     public CashAcceptor(int amount) {
         this.amount = amount;
     }
+    
     public int getAmount() {
         return amount;
     }
+    
     public void setAmount(int amount) {
         this.amount = amount;
     }
+    
     public void Action(){
 
     }
